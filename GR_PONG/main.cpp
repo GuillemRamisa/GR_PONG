22de3c39ac66069c8760b4ebca4ec9454a931ba3
Guillem Ramisa de Soto <guillemramisadesoto@gmail.com>
2023-12-28 18:07:15 +0100

    update publish

--- GR_PONG/main.cpp
+++ GR_PONG/main.cpp
@@ -3,13 +3,42 @@
 //  GR_PONG
 //
 //  Created by Guillem Ramisa de Soto on 28/12/23.
+//  Based on https://github.com/educ8s/Raylib-CPP-Starter-Template-for-VSCODE
 //
+// required linked Frameworks/files for compilation
+// - libraylib.a
+// - OpenGL
+// - CoreVideos
+// - IOkit
+// - Cocoa
+
 
 #include <iostream>
 #include "raylib.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main () {
+    
+    // Game globals
+    const int Screen_Width = 1280;
+    const int Screen_Height = 820;
+    SetTargetFPS(60);
+    
+    // init window
+    InitWindow(Screen_Width, Screen_Height, "GR_PONG");
+    
+    // Game loop where all the checks and events happen
+    // While statement to define the close state while is not closed, run -->
+    // function WindowShouldClose() operates with an Escape key or Close button on window
+    while (WindowShouldClose() == false) {
+        BeginDrawing();
+        
+        EndDrawing();
+    }
+    
+    
+    // close window
+    CloseWindow();
     return 0;
 }
